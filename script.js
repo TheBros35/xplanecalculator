33d3c02f9cefa82e4d9d0390c8fd5f725d034b46
Brandon Ambrose <gammaray558@gmail.com>
2020-04-06 19:13:38 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+/*
+function RealTimeETA(){
+    //take input from time fields, change to number type
+    var hour = document.getElementById("hourInp").value;
+    hour = Number(hour);
+    var minutes = document.getElementById("minInp").value;
+    minutes = Number(minutes);
+    var compression = document.getElementsByName('compressionRadio').value;
+    //compression = Number(compression);
+
+    //converts hours to minutes, adds them together
+    var allMinutes = (hour * 60) + minutes;
+    //converts allminutes to compressed time
+    allMinutes /= compression;
+
+    //pulls the remainder of minutes out of total hours
+    var convertedMinutes = allMinutes % 60;
+    //converts the minutes to hours
+    var convertedHours = Math.floor(allMinutes / 60);
+
+    RealTimer(convertedHours,convertedMinutes);
+
+    var final = convertedHours + " hours " + convertedMinutes + " minutes";
+    document.getElementById("realTimeH2").innerHTML = final;
+}
+*/
+
+$(document).ready(function(){
+    $("#realTimeBtn").click(function(){
+        var hour = $("#hourInp").val();
+        hour = Number(hour);
+        var minutes = $("#minInp").val();
+        minutes = Number(minutes);
+        var compression = $("input[name='compressionRadio']:checked").val();
+        console.log(compression);
+
+        //converts hours to minutes, adds them together
+        var allMinutes = (hour * 60) + minutes;
+        //converts allminutes to compressed time
+        allMinutes /= compression;
+
+        //pulls the remainder of minutes out of total hours
+        var convertedMinutes = allMinutes % 60;
+        //converts the minutes to hours
+        var convertedHours = Math.floor(allMinutes / 60);
+
+        //reloads the page if checkbox is changed due to shit timer code
+        $('#wantTimerId2').on('change', function() {
+            location.reload();
+        });
+        //runs the timer only if the checkbox is checked
+        if (document.getElementById("wantTimerId2").checked){
+            RealTimer(convertedHours,convertedMinutes);
+        }
+        
+        //displays the final result on the page
+        convertedMinutes = Math.ceil(convertedMinutes);
+        var final = convertedHours + " hours " + convertedMinutes + " minutes";
+        $("#realTimeH2").html(final);
+    });
+});
+
+
+function RealTimer(hours,minutes){
+    //converts the inputs to seconds
+    hours = hours * 60 * 60;
+    minutes = minutes * 60;
+    totalSeconds = hours + minutes;
+
+    var timer = new Timer();
+    timer.start({countdown: true, startValues: {seconds: totalSeconds}});
+    $('#realTimeH2').html(timer.getTimeValues().toString());
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#realTimeH2').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('targetAchieved', function (e) {
+        $('#realTimeH2').html('KABOOM!!');
+        var x = document.getElementById("timerStopAudio");
+        x.play();
+    });
+}
