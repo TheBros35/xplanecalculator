dd11b7ed207e1e30c7668919dd1b862efe9d97d2
Brandon Ambrose <bpambrose97@gmail.com>
2021-01-17 10:53:11 -0500

    deleted deps, refactored script

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 $(document).ready(function(){
-    $("#dropdown2x").click(function(){
-        sessionStorage.setItem("compression","2");
+    $('#dropdown2x').click(function(){
+        sessionStorage.setItem('compression','2');
     });
-    $("#dropdown4x").click(function(){
-        sessionStorage.setItem("compression","4");
+    $('#dropdown4x').click(function(){
+        sessionStorage.setItem('compression','4');
     });
-    $("#dropdown8x").click(function(){
-        sessionStorage.setItem("compression","8");
+    $('#dropdown8x').click(function(){
+        sessionStorage.setItem('compression','8');
     });
-    $("#dropdown16x").click(function(){
-        sessionStorage.setItem("compression","16");
+    $('#dropdown16x').click(function(){
+        sessionStorage.setItem('compression','16');
     });
-    $("#realTimeBtn").click(function(){
-        let hour = $("#hourInp").val();
-        hour = Number(hour);
-        let minutes = $("#minInp").val();
-        minutes = Number(minutes);
-        let compression = sessionStorage.getItem("compression");
-        compression = Number(compression);
-        console.log(hour + " " + minutes + " " + compression);
+    $('#realTimeBtn').click(function(){
+        const hour = Number($('#hourInp').val());
+        const minutes = Number($('#minInp').val());
+        const compression = Number(sessionStorage.getItem('compression'));
 
-        //converts hours to minutes, adds them together
-        let allMinutes = (hour * 60) + minutes;
-        //converts allminutes to compressed time
-        allMinutes /= compression;
-
+        //converts hours to minutes, adds them together, then divides that by the compression factor
+        const allMinutes = ((hour * 60) + minutes) / compression;
         //pulls the remainder of minutes out of total hours
-        let convertedMinutes = allMinutes % 60;
+        const convertedMinutes = Math.ceil(allMinutes % 60);
         //converts the minutes to hours
         const convertedHours = Math.floor(allMinutes / 60);
         
         //displays the final result on the page
-        convertedMinutes = Math.ceil(convertedMinutes);
-        let final = convertedHours + " hours " + convertedMinutes + " minutes";
-        $("#realTimeH2").html(final);
+        const final = convertedHours + ' hours ' + convertedMinutes + ' minutes';
+        $('#realTimeH2').html(final);
     });
 });
 
@@ -52,7 +44,7 @@ function RealTimer(hours,minutes){
     });
     timer.addEventListener('targetAchieved', function (e) {
         $('#realTimeH2').html('KABOOM!!');
-        let x = document.html("timerStopAudio");
+        let x = document.html('timerStopAudio');
         x.play();
     });
 }
