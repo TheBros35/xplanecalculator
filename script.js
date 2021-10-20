571dca389bae8efbf68749dc458b651bdc1fb23f
Brandon Ambrose <bpambrose97@gmail.com>
2021-10-19 20:43:08 -0400

    modified a few pieces of the frontend

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/*
-$('#dropdown2x').click(function(){
-    sessionStorage.setItem('compression','2');
-});
-$('#dropdown4x').click(function(){
-    sessionStorage.setItem('compression','4');
-});
-$('#dropdown8x').click(function(){
-    sessionStorage.setItem('compression','8');
-});
-$('#dropdown16x').click(function(){
-    sessionStorage.setItem('compression','16');
-});
-$('#realTimeBtn').click(function(){
-    const hour = Number($('#hourInp').val());
-    const minutes = Number($('#minInp').val());
-    const compression = Number(sessionStorage.getItem('compression'));
-
-    //converts hours to minutes, adds them together, then divides that by the compression factor
-    const allMinutes = ((hour * 60) + minutes) / compression;
-    //pulls the remainder of minutes out of total hours
-    const convertedMinutes = Math.ceil(allMinutes % 60);
-    //converts the minutes to hours
-    const convertedHours = Math.floor(allMinutes / 60);
-
-    //displays the final result on the page
-    const final = convertedHours + ' hours ' + convertedMinutes + ' minutes';
-    $('#realTimeH2').html(final);
-});
-*/
-
 function dropdown2x() {sessionStorage.setItem('compression','2');}
 
 function dropdown4x() {sessionStorage.setItem('compression','4');}
@@ -51,5 +20,5 @@ function calcRealTime(){
 
     //displays the final result on the page
     const final = convertedHours + ' hours ' + convertedMinutes + ' minutes';
-    document.getElementById('realTimeH2').value = final;
+    document.getElementById('realTimeH2').innerHTML = final;
 }
